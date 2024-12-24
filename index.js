285c51f1dfc46dcfe05b803d8c171cf7d39ac4e0
yeinerAguirre <yeineraguirre36@gmail.com>
2024-12-24 10:27:59 -0500

    Mensaje de cambio

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // Configuración básica de CORS
 app.use(cors({
-    origin: 'http://localhost:5173', // URL de tu frontend en React
+    origin: 'https://camara-front.vercel.app/', // URL de tu frontend en React
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
